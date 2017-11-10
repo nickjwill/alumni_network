2350eb3785e26bf65925013fd560552aed73bd89
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-10 13:59:24 -0700

    show styles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,18 @@
 class ProfilesController < ApplicationController
+
+    def show
+    end
+
+    def index
+    end
+    
+    def edit
+    end
+
+    def new
+    end
+
+    def create
+    end
+
 end
